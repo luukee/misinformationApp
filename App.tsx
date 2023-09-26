e57de7d602bc2b072b00c9963e5d3a58a811b876
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-09-26 15:48:08 -0700

    replace App.tsx with og App.js

--- App.tsx
+++ App.tsx
@@ -1,118 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {Provider as StateProvider} from 'react-redux';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import RootRoute from './src/routes/root.routes';
+import store from './src/redux/store';
+import './src/constants/IMLocalize';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  global.screens = {
+    login: 'LoginScreen',
+    signUp: 'SignupScreen',
+    // selectLanguageBeforeSignUp:'SelectLanguageScreen',
+    forgotPassword: 'ForgotPassword',
+    forgotPasswordSuccess: 'ForgotPasswordSuccess',
+    changeEmail: 'ChangeEmail',
+    changePassword: 'ChangePassword',
+    home: 'HomeScreen',
+    reminder: 'Reminder',
+    setting: 'SettingScreen',
+    talkingAbout: 'TalkingAboutScreen',
+    whereTopic: 'WhereTopic',
+    seeFrom: 'SeeFromScreen',
+    agencySelect: 'AgencySelect',
+    reportCreation: 'ReportCreation',
+    verificationSent: 'VerificationSentScreen',
+    passwordVerificationSent: 'PasswordVerificationSentScreen',
+    reportDetail: 'ReportDetail',
+    reportSubmittedScreen: 'ReportSubmittedScreen',
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  global.routes = {
+    authRoute: 'AuthRoute',
+    appRoute: 'AppRoute',
+    bottomTabRoute: 'BottomTabRoute',
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <StateProvider store={store}>
+      <RootRoute />
+    </StateProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
