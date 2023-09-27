094f3ccffd0a717d5a4b2285659458962c82fca9
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-09-27 06:32:46 -0700

    WORKING : default react native

--- App.tsx
+++ App.tsx
@@ -1,41 +1,118 @@
-import {Provider as StateProvider} from 'react-redux';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-import RootRoute from './src/routes/root.routes';
-import store from './src/redux/store';
-import './src/constants/IMLocalize';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-export default function App() {
-  global.screens = {
-    login: 'LoginScreen',
-    signUp: 'SignupScreen',
-    // selectLanguageBeforeSignUp:'SelectLanguageScreen',
-    forgotPassword: 'ForgotPassword',
-    forgotPasswordSuccess: 'ForgotPasswordSuccess',
-    changeEmail: 'ChangeEmail',
-    changePassword: 'ChangePassword',
-    home: 'HomeScreen',
-    reminder: 'Reminder',
-    setting: 'SettingScreen',
-    talkingAbout: 'TalkingAboutScreen',
-    whereTopic: 'WhereTopic',
-    seeFrom: 'SeeFromScreen',
-    agencySelect: 'AgencySelect',
-    reportCreation: 'ReportCreation',
-    verificationSent: 'VerificationSentScreen',
-    passwordVerificationSent: 'PasswordVerificationSentScreen',
-    reportDetail: 'ReportDetail',
-    reportSubmittedScreen: 'ReportSubmittedScreen',
-  };
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-  global.routes = {
-    authRoute: 'AuthRoute',
-    appRoute: 'AppRoute',
-    bottomTabRoute: 'BottomTabRoute',
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <StateProvider store={store}>
-      <RootRoute />
-    </StateProvider>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,6 @@
  *
  * @type {import('metro-config').MetroConfig}
  */
-const {getDefaultConfig} = require('expo/metro-config');
+const config = {};
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
